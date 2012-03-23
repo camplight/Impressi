61d8118ce70a95aff750036a916c5166780002cb
Silas Barta <sbarta@gmail.com>
2012-03-22 23:32:36 -0700

    finally working

--- app/assets/javascripts/pickle.js
+++ app/assets/javascripts/pickle.js
@@ -53,15 +53,16 @@ var sendViaAjax = function() {
 		 }
 	});
 };
+
 var reset = function() {
     i = window.impress();
     i.reset();
     window.impress();
-}
+};
 
 var buildTree = function(data) {
 	var template_attrs = ['class', 'data-rotate-x', 'data-rotate-y', 'data-rotate-z', 'data-scale', 'data-x', 'data-y', 'data-z'];
-  	var impress_divs = $('#impress > .step').remove();
+  	$('#impress > div').remove();
 	for(var i = 0; i < data.deck_data.length; i++) {
 		$('#impress').append('<div></div>');
 		for(var x = 0; x < template_attrs.length; x++) {
@@ -83,86 +84,88 @@ $(document).ready(function() {
 		success: function(data) {
 			buildTree(data);
 			reset();
+			
+			var current_slide = null,
+			 		mode = 'prezi';
+
+			document.addEventListener("keydown", function ( event ) {
+				if (mode == "edit"){
+					event.stopImmediatePropagation();
+				}
+			});
+
+			document.addEventListener("keyup", function ( event ) {
+				if (mode == "edit"){
+					event.stopImmediatePropagation();
+				}
+			});
+
+
+			$(function() {	
+				var activeInput = false,
+						textarea = document.createElement('textarea'),
+						inlineEditor = $(textarea);
+				inlineEditor.attr('rows', '3');
+				inlineEditor.attr('id', 'inline-editor');
+				inlineEditor.attr('placeholder', 'Click here to edit text.')
+
+				$(".editable").click(function(e) {
+
+					if(!$(this).hasClass('active')) {
+						return false;
+					}
+
+					current_slide = $(this);
+					currentText = current_slide.text().length == 0 ? "write stuff" : current_slide.text();
+					inlineEditor.val(currentText);
+					e.stopImmediatePropagation();
+
+					if(activeInput == false) {
+						activeInput = true;
+						mode = 'edit';
+						console.log(mode);
+						$(this).html(inlineEditor);
+						inlineEditor.focus();
+					} else {
+					 		activeInput = false;
+							mode = 'prezi';
+							inlineEditor.blur();
+							e.stopImmediatePropagation();
+					}
+
+					inlineEditor.on({	
+						keyup: function(e) {
+							if (e.keyCode == 27) {
+								$(this).blur();
+								activeInput = false;
+							}
+						},
+
+						click: function(e) {
+							e.stopPropagation();
+						},
+
+						blur: function(e) {
+							currentInput = $(this).val();
+							if($(this).val() === "" || $(this).val().length === 1) {
+								current_slide.text($(this).attr('placeholder'));
+								e.stopPropagation();
+							} else {
+								current_slide.text(currentInput);
+								e.stopImmediatePropagation();
+								$(this).val("");
+							}
+							mode = 'prezi';
+						}
+					});
+					return false;
+				});
+
+			});
 		}
 	});
 	
-	var current_slide = null,
-	 		mode = 'prezi';
 
-	document.addEventListener("keydown", function ( event ) {
-		if (mode == "edit"){
-			event.stopImmediatePropagation();
-		}
-	});
-
-	document.addEventListener("keyup", function ( event ) {
-		if (mode == "edit"){
-			event.stopImmediatePropagation();
-		}
-	});
-
-
-	$(function() {	
-		var activeInput = false,
-				textarea = document.createElement('textarea'),
-				inlineEditor = $(textarea);
-		inlineEditor.attr('rows', '3');
-		inlineEditor.attr('id', 'inline-editor');
-		inlineEditor.attr('placeholder', 'Click here to edit text.')
-
-		$(".editable").click(function(e) {
-
-			if(!$(this).hasClass('active')) {
-				return false;
-			}
-
-			current_slide = $(this);
-			currentText = current_slide.text().length == 0 ? "write stuff" : current_slide.text();
-			inlineEditor.val(currentText);
-			e.stopImmediatePropagation();
-
-			if(activeInput == false) {
-				activeInput = true;
-				mode = 'edit';
-				console.log(mode);
-				$(this).html(inlineEditor);
-				inlineEditor.focus();
-			} else {
-			 		activeInput = false;
-					mode = 'prezi';
-					inlineEditor.blur();
-					e.stopImmediatePropagation();
-			}
-
-			inlineEditor.on({	
-				keyup: function(e) {
-					if (e.keyCode == 27) {
-						$(this).blur();
-						activeInput = false;
-					}
-				},
-
-				click: function(e) {
-					e.stopPropagation();
-				},
-
-				blur: function(e) {
-					currentInput = $(this).val();
-					if($(this).val() === "" || $(this).val().length === 1) {
-						current_slide.text($(this).attr('placeholder'));
-						e.stopPropagation();
-					} else {
-						current_slide.text(currentInput);
-						e.stopImmediatePropagation();
-						$(this).val("");
-					}
-					mode = 'prezi';
-				}
-			});
-			return false;
-		});
-
-	});
 
 });
 
