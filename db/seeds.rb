5742bd52ae3e46c87a560c5d1c4323fd47e72cde
Craig Little <craiglttl@gmail.com>
2012-03-25 16:51:41 -0700

    really fix db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(:email => "admin@impressi.com", :password => "foobar", :password_confirmation => "foobar")
+t1 = Template.create('name'          => 'Righty',
+                     'klass'         => 'step slide editable',
+                     'data-x'        => 1000,
+                     'data-y'        => 0,
+                     'data-z'        => 0,
+                     'data-rotate-x' => 0,
+                     'data-rotate-y' => 0,
+                     'data-rotate-z' => 0,
+                     'data-scale'    => 1)
 
-t1 = Template.new('name'          => 'Righty',
-                  'klass'         => 'step slide editable',
-                  'data-x'        => 1000,
-                  'data-y'        => 0,
-                  'data-z'        => 0,
-                  'data-rotate-x' => 0,
-                  'data-rotate-y' => 0,
-                  'data-rotate-z' => 0,
-                  'data-scale'    => 1)
-
-t1 = Template.new('name'          => 'Uppy',
-                  'klass'         => 'step slide editable',
-                  'data-x'        => 0,
-                  'data-y'        => -1000,
-                  'data-z'        => 0,
-                  'data-rotate-x' => 0,
-                  'data-rotate-y' => 0,
-                  'data-rotate-z' => 0,
-                  'data-scale'    => 1)
+t1 = Template.create('name'          => 'Uppy',
+                    'klass'         => 'step slide editable',
+                    'data-x'        => 0,
+                    'data-y'        => -1000,
+                    'data-z'        => 0,
+                    'data-rotate-x' => 0,
+                    'data-rotate-y' => 0,
+                    'data-rotate-z' => 0,
+                    'data-scale'    => 1)
