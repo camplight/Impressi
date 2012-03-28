53085b5764f8f18b5af453c988ba526d9f81e99f
Craig Little <craiglttl@gmail.com>
2012-03-28 11:15:48 -0700

    basic user show page working'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   
   def show
-    redirect_to root_path unless current_user == User.find(params[:id])
+    redirect_to :root unless current_user == User.find(params[:id])
     @decks = current_user.decks
   end
   
