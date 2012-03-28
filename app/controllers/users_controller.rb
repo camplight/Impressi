713bac091cd4f2518fe7d60f2e78942c262a8d1b
Silas Barta <sbarta@gmail.com>
2012-03-28 15:16:04 -0700

    removed debugging info from user:show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   
   def show
-    logger.info("~"*30)
-    logger.info(current_user.inspect)
-    logger.info("~"*30)
     redirect_to :root unless current_user == User.find(params[:id])
     @decks = current_user.decks.sort
     @deck = current_user.decks.build
